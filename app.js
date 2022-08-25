76013c29cd517dcfcfc605d072a2f6bb8c2c745e
EsmeNg <nwmx3x@gmail.com>
2022-08-25 21:23:56 +0800

    feat: create index page

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ const exphbs = require('express-handlebars')
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/users')
 const db = mongoose.connection
 
-db.on('error', (error) => {
+const User = require('./models/user')
+const { count } = require('./models/user')
+
+db.on('error', error => {
   console.log(error)
 })
 
@@ -20,8 +25,25 @@ db.once('open', () => {
   console.log(`mongodb is running!`)
 })
 
+let email = ''
+let loginFailed = false
+
 app.get('/', (req, res) => {
-  res.render('login')
+  res.render('login', { loginFailed, email })
+})
+
+app.post('/login', (req, res) => {
+  email = req.body.email
+  return User.findOne(req.body)
+    .lean()
+    .then(user => {
+      if (user === null) {
+        loginFailed = true
+        return res.redirect('/')
+      }
+      const firstName = user.firstName
+      res.render('index', { firstName, email })
+    })
 })
 
 app.listen(port, () => {
